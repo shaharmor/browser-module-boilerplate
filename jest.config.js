a332129642e0abda3327c66da30fd5bc6b7fb6da
Shahar Mor <shaharmor1@gmail.com>
2020-02-10 17:31:30 +0200

    chore(repo): bump deps

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,11 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const { pathsToModuleNameMapper } = require('ts-jest/utils');
 const mergeOptions = require('merge-options');
 const jestTSJestPreset = require('ts-jest/jest-preset');
-const jestPuppeteerPreset = require('jest-puppeteer/jest-preset');
 const { compilerOptions } = require('./tsconfig');
 
 module.exports = mergeOptions(
   {
     moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
   },
-  jestTSJestPreset,
-  jestPuppeteerPreset
+  jestTSJestPreset
 );

a9135881747853c84fac7ce957205d5b21e1bcda
Shahar Mor <shaharmor1@gmail.com>
2020-10-29 10:17:10 +0200

    webpack 5

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from '@jest/types';
+import { pathsToModuleNameMapper } from 'ts-jest/utils';
+import * as tsConfig from './tsconfig.json';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  moduleNameMapper: pathsToModuleNameMapper(tsConfig.compilerOptions.paths, { prefix: '<rootDir>/' }),
+};
+
+export default config;
--- webpack.config.ts
+++ webpack.config.ts
@@ -3,6 +3,7 @@ import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
 import webpack from 'webpack';
 import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 
 // eslint-disable-next-line no-underscore-dangle
 const __DEV__ = process.env.NODE_ENV === 'development';
@@ -39,8 +40,9 @@ const config: webpack.Configuration & { devServer: WebpackDevServerConfiguration
     ],
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new webpack.SourceMapDevToolPlugin({
-      filename: null,
+      filename: undefined,
       exclude: [/node_modules/],
       test: /\.ts($|\?)/i,
     }),
@@ -49,8 +51,8 @@ const config: webpack.Configuration & { devServer: WebpackDevServerConfiguration
   devServer: {
     compress: true,
     open: true,
+    injectClient: false, // https://github.com/webpack/webpack-dev-server/issues/2484#issuecomment-655211893
   },
 };
 
-// eslint-disable-next-line import/no-default-export
 export default config;
